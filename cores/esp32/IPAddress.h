426a4260d81569adf8e42d62b4a17cc80a7f738a
Sly Gryphon <sgryphon@computer.org>
2022-08-26 16:34:43 +1000

    Make unit32_t constructor explicit

--- cores/esp32/IPAddress.h
+++ cores/esp32/IPAddress.h
@@ -47,7 +47,7 @@ public:
     // Constructors
     IPAddress();
     IPAddress(uint8_t first_octet, uint8_t second_octet, uint8_t third_octet, uint8_t fourth_octet);
-    IPAddress(uint32_t address);
+    explicit IPAddress(uint32_t address);
     IPAddress(const uint8_t *address);
     virtual ~IPAddress() {}
 
